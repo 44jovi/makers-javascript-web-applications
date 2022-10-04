46b8391e79921bab1630c01b812529be4d502510
Jovi <kwong.jovi@gmail.com>
2022-10-04 10:50:04 +0100

    phase2 bite5 bite6

---
+++ phase2/bite5_bite6/example-2/view.test.js
@@ -0,0 +1,33 @@
+/**
+ * @jest-environment jsdom
+ */
+
+const fs = require('fs'); // to read non-JS files
+const View = require('./view');
+
+describe('Page view', () => {
+  it('displays 2 paragraphs', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+    const view = new View();
+
+    expect(document.querySelectorAll('p').length).toBe(2);
+  });
+
+  it('adds a paragraph', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+    const view = new View();
+
+    view.addParagraph();
+
+    expect(document.querySelectorAll('p').length).toBe(3);
+  });
+  
+  it('removes all paragraphs', () => {
+    document.body.innerHTML = fs.readFileSync('./index.html');
+    const view = new View();
+
+    view.clearParagraphs();
+    
+    expect(document.querySelectorAll('p').length).toBe(0);
+  });
+});
