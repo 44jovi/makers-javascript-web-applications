884b59710f16c120029bee17ff3b1710781c6049
Jovi <kwong.jovi@gmail.com>
2022-10-04 13:28:18 +0100

    phase2 bite7 exercise1

--- phase2/bite7/notes-app/bundle.js
+++ phase2/bite7/notes-app/bundle.js
@@ -25,10 +25,36 @@
     }
   });
 
+  // notesView.js
+  var require_notesView = __commonJS({
+    "notesView.js"(exports, module) {
+      var NotesView2 = class {
+        constructor(model2) {
+          this.model = model2;
+          this.mainContainerEl = document.querySelector("#main-container");
+        }
+        displayNotes() {
+          const allNotes = this.model.getNotes();
+          allNotes.forEach((note) => {
+            const noteEl = document.createElement("div");
+            noteEl.textContent = note;
+            noteEl.className = "note";
+            this.mainContainerEl.append(noteEl);
+          });
+        }
+      };
+      module.exports = NotesView2;
+    }
+  });
+
   // index.js
   var NotesModel = require_notesModel();
+  var NotesView = require_notesView();
   var model = new NotesModel();
+  var view = new NotesView(model);
   console.log("The notes app is running");
   console.log(new Date());
-  console.log(model.getNotes());
+  model.addNote("note 1");
+  model.addNote("note 2");
+  view.displayNotes();
 })();
--- phase2/bite7/notes-app/index.js
+++ phase2/bite7/notes-app/index.js
@@ -1,14 +1,12 @@
-// console.log doesn't print anything on the page
-// it is not meant to be visible to the user, but for you
-// to help in debugging and getting visibility in your JS code.
-//
-// on Mac (using Chrome), use Option+Command+J to open the console and see this message.
-
 const NotesModel = require('./notesModel');
+const NotesView = require('./notesView');
 
 const model = new NotesModel();
+const view = new NotesView(model);
 
 console.log('The notes app is running');
 console.log(new Date());
 
-console.log(model.getNotes());
+model.addNote('note 1');
+model.addNote('note 2');
+view.displayNotes();
