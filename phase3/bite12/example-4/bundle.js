69a93635ea69429023735ebf7d212f7d864b5f9f
Jovi <kwong.jovi@gmail.com>
2022-10-05 12:11:58 +0100

    phase3 bite 12 - completed

--- phase3/bite12/example-4/bundle.js
+++ phase3/bite12/example-4/bundle.js
@@ -47,11 +47,20 @@
           submitButtonEl.addEventListener("click", () => {
             const repoName = repoInputEl.value;
             this.api.getRepoInfo(repoName, (repoData) => {
+              this.model.setRepoInfo(repoData);
+              this.display();
               console.log(repoData);
+              console.log("button clicked - #submit-button");
             });
           });
         }
         display() {
+          const repoData = this.model.getRepoInfo();
+          const repoNameEl = document.querySelector("#repo-name");
+          repoNameEl.append(repoData.full_name);
+          const repoDescripEl = document.querySelector("#repo-description");
+          repoDescripEl.append(repoData.description);
+          document.getElementById("repo-profile-img").src = repoData.organization.avatar_url;
         }
       };
       module.exports = GithubView2;
