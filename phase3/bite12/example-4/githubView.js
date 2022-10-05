99b78784776b743c8aa7fea209a38d27cdbc6da0
Jovi <kwong.jovi@gmail.com>
2022-10-05 10:51:08 +0100

    phase3 bite12 - setup

---
+++ phase3/bite12/example-4/githubView.js
@@ -0,0 +1,23 @@
+class GithubView {
+  constructor(model, api) {
+    this.model = model;
+    this.api = api;
+
+    const submitButtonEl = document.querySelector('#submit-button');
+    const repoInputEl = document.querySelector('#repo-name-input');
+
+    submitButtonEl.addEventListener('click', () => {
+      const repoName = repoInputEl.value;
+
+      this.api.getRepoInfo(repoName, repoData => {
+        console.log(repoData);
+      });
+    });
+  }
+
+  display() {
+
+  }
+}
+
+module.exports = GithubView;
