6693bd8644f46da5762bde07145d90fc246a713b
Jovi <kwong.jovi@gmail.com>
2022-10-07 16:32:45 +0100

    refactor(notesApi.test.js) - use correct fetch mock function to disable mocking on specific tests. Pass tests. Remove redundant separate test file

--- phase3/bite15/notesApi.test.js
+++ phase3/bite15/notesApi.test.js
@@ -2,13 +2,12 @@ const NotesApi = require('./notesApi');
 
 require('jest-fetch-mock').enableMocks()
 
-beforeEach(() => {
-  fetch.resetMocks();
-});
+// beforeEach(() => {
+//   fetch.resetMocks();
+// });
 
 describe('NotesApi class', () => {
   it('calls fetch and loads data', (done) => {
-    
     const api = new NotesApi();
 
     fetch.mockResponseOnce(JSON.stringify({
@@ -21,5 +20,24 @@ describe('NotesApi class', () => {
       expect(returnedData.content).toEqual(['Fake note!']);
       done();
     });
+  });
+
+  it('#createNote adds a note to the server', (done) => {
+    // for now use real API to confirm note added
+    fetchMock.dontMock();
+
+    const api = new NotesApi();
+
+    api.createNote('New remote note');
+
+    const test = () => {
+      api.loadNotes((returnedData) => {
+      console.log('Test running');
+      console.log(returnedData);
+      expect(returnedData.slice(-1)).toEqual(['New remote note']);
+      done();
+    })}
+
+    setTimeout(test, 1000);
   });
 });
